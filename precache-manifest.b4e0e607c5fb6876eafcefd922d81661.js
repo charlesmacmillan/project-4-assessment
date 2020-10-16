c94553da33574cb1582e78d75598ce78b57bb8f3
Charles MacMillan <charlesmacmillanpro@gmail.com>
2020-10-16 13:50:52 -0600

    Updates

---
+++ precache-manifest.b4e0e607c5fb6876eafcefd922d81661.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b847cb3997f35749b9b5fa42214db5b2",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "bf7d5c3ce709f5a38cac",
+    "url": "/project-4-assessment/static/css/main.c0835aaf.chunk.css"
+  },
+  {
+    "revision": "608118624ae7ee197d4b",
+    "url": "/project-4-assessment/static/js/2.e208ecfd.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/project-4-assessment/static/js/2.e208ecfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf7d5c3ce709f5a38cac",
+    "url": "/project-4-assessment/static/js/main.ef1cb218.chunk.js"
+  },
+  {
+    "revision": "5f6f705e1e3b3b948cc9",
+    "url": "/project-4-assessment/static/js/runtime-main.3bb3fb77.js"
+  }
+]);
